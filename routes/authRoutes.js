da59557d282551de13ae430ddf3e7fbe95634408
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2024-05-08 00:45:31 -0400

    fixed api, I think

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -12,9 +12,9 @@ authRouter.use(cors({
 }));
 
 // API - Routes
-authRouter.post('/api/register', registerUser);
-authRouter.post('/api/login', loginUser);
-authRouter.get('/api/profile', getProfile);
-authRouter.get('/api/user', getUser);
+authRouter.post('/register', registerUser);
+authRouter.post('/login', loginUser);
+authRouter.get('/profile', getProfile);
+authRouter.get('/user', getUser);
 
 export default authRouter;
