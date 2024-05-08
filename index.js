fb79b2a516d9c8e7028d76d21a9bf42f86f92162
oddballadmin <61040185+oddballadmin@users.noreply.github.com>
2024-05-08 00:13:42 -0400

    chore: Update route for API endpoint

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use('/', authRouter);
 app.use('/', expenseRouter);
 app.use('/', incomeRouter);
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the Budget App API');
 });
 
